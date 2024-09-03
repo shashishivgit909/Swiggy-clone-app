f98c6d71a1b000d48d0ef56c7266024d9bbd6298
SHASHI KALWAR <138351592+shashishivgit909@users.noreply.github.com>
2024-09-03 22:34:06 +0530

    changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,7 +5,7 @@ import foodItems from "../assets/data/gofood.food_items.js";
 
 function Category() {
     const [categories, setCategories] = useState([]);
-
+    const [slide, setSlide] = useState(0);
     const fetchCategories = async () => {
         //  const response=await fetch("http://localhost:5000/categories");
         //  const data=response.json();
@@ -18,26 +18,39 @@ function Category() {
 
     useEffect(() => {
         fetchCategories();
-    }, [])
+    }, []);
+
+    const prevSlide = () => {
+        console.log(slide, "hehgccec")
+        setSlide(slide - 3);
+    }
+
+    const nextSlide = () => {
+        setSlide(slide + 3);
+        console.log("nextSlide", slide)
+    }
+
     return (
         <>
             <div className='max-w-[1200px] mx-auto'>
                 <div className='flex justify-between my-3'>
                     <div className='font-bold text-xl'>What's on your mind ?</div>
                     <div className='flex gap-3'>
-                        <div className='cursor-pointer w-[30px] h-[30px] bg-[#bdc7cf] rounded-full flex items-center justify-center'> <FaArrowLeft />  </div>
-                        <div className='cursor-pointer w-[30px] h-[30px] bg-[#bdc7cf] rounded-full flex items-center justify-center'> <FaArrowRight /> </div>
+                        <div className='cursor-pointer w-[30px] h-[30px] bg-[#bdc7cf] rounded-full flex items-center justify-center' onClick={prevSlide}> < FaArrowLeft />  </div>
+                        <div className='cursor-pointer w-[30px] h-[30px] bg-[#bdc7cf] rounded-full flex items-center justify-center' onClick={nextSlide}> <FaArrowRight /> </div>
                     </div>
                 </div>
 
 
-  {/* tn flex , if wrap not given then items shrink to fit in conatiner wodth irrespect of their individual width so if we want them to appear in their actual width then either give wrap (then they comes in next line , but if want to keep in single line then make shrink-0 by only some elemnt comes in conatiner and other got over-flow*/}
-                <div className='flex border border-red-600 shrink-0 overflow-hidden'>
+                {/* tn flex , if wrap not given then items shrink to fit in conatiner wodth irrespect of their individual width so if we want them to appear in their actual width then either give wrap (then they comes in next line , but if want to keep in single line then make shrink-0 by only some elemnt comes in conatiner and other got over-flow*/}
+                <div className='flex  w-full border border-red-600 shrink-0 overflow-hidden'>
                     {
                         categories.map((item, index) => {
                             return (
-                                <div key={index} className='shrink-0'>
-                                    <img className="h-[220px] w-[240px]" src={item.img} alt="" />
+                                // <div key={index} className='shrink-0' style={{transform: `translateX(-40px)`}}>
+                                <div key={index} className='shrink-0' style={{ transform: `translateX(${slide*150}px)` }} >
+                                    {console.log(`-${slide*150}px`)}
+                                    <img className="h-[180px] w-[150px]" src={item.img} alt="" />
                                 </div>
                             )
                         })
